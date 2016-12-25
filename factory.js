7262c5b2cbe67cabb770826f19e952260a959e19
Rick Lancee <rickondraw@gmail.com>
2016-12-25 13:13:22 +0100

    refactor to ES6

--- factory.js
+++ factory.js
@@ -1,29 +1,10 @@
 'use strict';
 
 const factory = function() {
-    const extend = function(object) {
-        const extendees = Array.prototype.slice.call(arguments, 1);
-
-        const length = extendees.length;
-        for (let i = 0; i < length; i++) {
-            const source = extendees[i];
-            const keys = Object.keys(source);
-            const keyLength = keys.length;
-
-            for (let i = 0; i < keyLength; i++) {
-                object[keys[i]] = source[keys[i]];
-            }
-        }
-
-        return object;
-    };
-
     return {
-        compose: function() {
-            return extend.apply(null, arguments);
-        },
+        compose: (...objects) => Object.assign(...objects),
         create: function(func) {
-            var obj = {};
+            const obj = {};
             func.call(obj);
             return obj;
         }
